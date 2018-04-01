e894b1ae067ceb8b286a5681d25e79d092196888
NaJ64 <njudice@gmail.com>
2018-03-31 23:10:27 -0500

    updated server side vessel model to remove some calculated props

--- Ballast.Core/src/Server/Models/Vessel.cs
+++ Ballast.Core/src/Server/Models/Vessel.cs
@@ -12,12 +12,6 @@ namespace Ballast.Core.Models
         public VesselType VesselType { get; set; }
         public Player Captain { get; set; }
         public Player Radioman { get; set; }
-        public VesselPosition StartingPosition => PositionHistory
-            .OrderBy(x => x.ArrivalUtc)
-            .FirstOrDefault();
-        public VesselPosition CurrentPosition => PositionHistory
-            .OrderByDescending(x => x.ArrivalUtc)
-            .FirstOrDefault();
         public IEnumerable<VesselPosition> PositionHistory { get; set; }
         public int RemainingHP { get; set; }
         public int Kills { get; set; }
