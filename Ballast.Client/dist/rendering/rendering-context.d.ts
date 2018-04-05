39331eba4203f398b89c3c75e30aacb6b857e2ca
NaJ64 <njudice@gmail.com>
2018-04-04 21:35:55 -0500

    added event bus implementation with some basic tests

--- Ballast.Client/dist/rendering/rendering-context.d.ts
+++ Ballast.Client/dist/rendering/rendering-context.d.ts
@@ -1,12 +1,10 @@
 import * as THREE from 'three';
 export declare class RenderingContext {
     readonly canvas: HTMLCanvasElement;
-    readonly canvas2dContext?: CanvasRenderingContext2D;
     readonly threeWebGLRenderer?: THREE.WebGLRenderer;
     readonly threePerspectiveCamera?: THREE.PerspectiveCamera;
     readonly threeScene?: THREE.Scene;
     constructor(canvas: HTMLCanvasElement);
-    private create2dContext(canvas);
     private createRenderer(canvas);
     private createScene();
     private createCamera(canvas);
---
+++ Ballast.Client/src/messaging/event-bus.test.ts
@@ -0,0 +1,56 @@
+import 'jest';
+import 'reflect-metadata';
+import { IEvent } from './ievent';
+import { EventBus } from './event-bus';
+
+
+let handled = 0;
+let eventKey = Symbol.for('TestEvent');
+let event: IEvent = { value: 'test' };
+let handler1 = (event: IEvent) => {
+    handled += 1;
+};
+let handler2 = (event: IEvent) => {
+    handled += 2;
+};
+let eventBus = new EventBus();
+
+test('returns empty list for unsubscribed', () => {
+    let handlers = eventBus.getHandlers(eventKey);
+    expect(handlers.length).toBeLessThanOrEqual(0);
+    expect(handlers).not.toContain(handler1);
+    expect(handlers).not.toContain(handler2);
+    expect(handlers.length).toBeLessThan(1);
+});
+
+test('accepts new subscription(s)', () => {
+    let handlers = eventBus.getHandlers(eventKey);
+    expect(handlers).not.toContain(handler1);
+    eventBus.subscribe<IEvent>(eventKey, handler1);
+    handlers = eventBus.getHandlers<IEvent>(eventKey);
+    expect(handlers).toContain(handler1);
+    expect(handlers).not.toContain(handler2);
+    eventBus.subscribe<IEvent>(eventKey, handler2);
+    handlers = eventBus.getHandlers<IEvent>(eventKey);
+    expect(handlers).toContain(handler2);
+});
+
+test('publishes event to all subscribers', () => {
+    expect(handled).toEqual(0);
+    eventBus.publish(eventKey, event);
+    expect(handled).toEqual(3); // handled by both 1 + 2
+    handled = 0;
+}, 1000);
+
+test('honors subscription removal', () => {
+    let handlers = eventBus.getHandlers<IEvent>(eventKey);
+    expect(handlers).toContain(handler1);
+    expect(handlers).toContain(handler2);
+    eventBus.unsubscribe(eventKey, handler1);
+    handlers = eventBus.getHandlers<IEvent>(eventKey);
+    expect(handlers).not.toContain(handler1);
+    expect(handlers).toContain(handler2);
+    expect(handled).toEqual(0);
+    eventBus.publish(eventKey, event);
+    expect(handled).toEqual(2); // handled by 2 only
+});
