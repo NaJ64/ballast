7fe929c2aedff257f71ef8637f97141f5c3fc980
Nate Judice <njudice@gmail.com>
2018-04-12 17:25:56 -0500

    expanded properties of defined tile shapes & stubbed out board generator

--- Ballast.Core/src/Client/models/tile-shape.ts
+++ Ballast.Core/src/Client/models/tile-shape.ts
@@ -1,20 +1,82 @@
 export interface ITileShape { 
     value: number;
     name: string;
+    applyHexRowScaling?: boolean;
+    doubleIncrement?: boolean;
+    hasDirectionNorth?: boolean;
+    hasDirectionSouth?: boolean;
+    hasDirectionWest?: boolean;
+    hasDirectionEast?: boolean;
+    hasDirectionNorthWest?: boolean;
+    hasDirectionNorthEast?: boolean;
+    hasDirectionSouthWest?: boolean;
+    hasDirectionSouthEast?: boolean;
 }
 
 export class TileShape implements ITileShape {
 
-    public static Square: TileShape = new TileShape({ value: 0, name: 'Square' });
-    public static Octagonal: TileShape = new TileShape({ value: 1, name: 'Octagonal' });
-    public static Hexagonal: TileShape = new TileShape({ value: 2, name: 'Hexagonal' });
+    public static Square: TileShape = new TileShape({ 
+        value: 0, 
+        name: 'Square',
+        doubleIncrement: true,
+        hasDirectionNorth: true,
+        hasDirectionSouth: true,
+        hasDirectionWest: true,
+        hasDirectionEast: true
+    });
+
+    public static Octagonal: TileShape = new TileShape({ 
+        value: 1, 
+        name: 'Octagonal',
+        doubleIncrement: true,
+        hasDirectionNorth: true,
+        hasDirectionSouth: true,
+        hasDirectionWest: true,
+        hasDirectionEast: true,
+        hasDirectionNorthWest: true,
+        hasDirectionNorthEast: true,
+        hasDirectionSouthWest: true,
+        hasDirectionSouthEast: true
+    });
+
+    public static Hexagonal: TileShape = new TileShape({ 
+        value: 2, 
+        name: 'Hexagonal',
+        applyHexRowScaling: true,
+        hasDirectionWest: true,
+        hasDirectionEast: true,
+        hasDirectionNorthWest: true,
+        hasDirectionNorthEast: true,
+        hasDirectionSouthWest: true,
+        hasDirectionSouthEast: true
+    });
     
     public readonly value: number;
     public readonly name: string;
+    public readonly applyHexRowScaling: boolean;
+    public readonly doubleIncrement: boolean;
+    public readonly hasDirectionNorth: boolean;
+    public readonly hasDirectionSouth: boolean;
+    public readonly hasDirectionWest: boolean;
+    public readonly hasDirectionEast: boolean;
+    public readonly hasDirectionNorthWest: boolean;
+    public readonly hasDirectionNorthEast: boolean;
+    public readonly hasDirectionSouthWest: boolean;
+    public readonly hasDirectionSouthEast: boolean;
 
     private constructor(state: ITileShape) {
         this.value = state.value;
         this.name = state.name;
+        this.applyHexRowScaling = !!state.applyHexRowScaling;
+        this.doubleIncrement = !!state.doubleIncrement;
+        this.hasDirectionNorth = !!state.hasDirectionNorth;
+        this.hasDirectionSouth = !!state.hasDirectionSouth;
+        this.hasDirectionWest = !!state.hasDirectionWest;
+        this.hasDirectionEast = !!state.hasDirectionEast;
+        this.hasDirectionNorthWest = !!state.hasDirectionNorthWest;
+        this.hasDirectionNorthEast = !!state.hasDirectionNorthEast;
+        this.hasDirectionSouthWest = !!state.hasDirectionSouthWest;
+        this.hasDirectionSouthEast = !!state.hasDirectionSouthEast;
     }
 
     public static list(): TileShape[] {
--- Ballast.Core/src/Client/services/board-generator.ts
+++ Ballast.Core/src/Client/services/board-generator.ts
@@ -1,16 +1,58 @@
 import { IBoard, Board } from '../models/board';
 import { IBoardType, BoardType } from '../models/board-type';
+import { ITile, Tile } from '../models/tile';
 import { ITileShape, TileShape } from '../models/tile-shape';
+import { IAxialCoordinates, AxialCoordinates } from '../models/axial-coordinates';
+import { ICubicCoordinates, CubicCoordinates } from '../models/cubic-coordinates';
+import { IOffsetCoordinates, OffsetCoordinates} from '../models/offset-coordinates';
 
 export class BoardGenerator {
+
     public createBoard(tileShape: ITileShape, boardType: IBoardType, sizeWidth: number, sizeHeight?: number) {
+
         let useTileShape = TileShape.fromObject(tileShape);
         let useBoardType = BoardType.fromObject(boardType);   
+
         let width = sizeWidth;
         let height = sizeHeight || sizeWidth;  
         if (useBoardType.equals(BoardType.RegularPolygon) && sizeWidth != sizeHeight) {
             throw new Error('Regular polygon board(s) cannot specify different width/height values');
         }
-        // TODO:  Create tiles collection here
+
+        // Rectangular board (fixed number of tiles per row)
+        if (useBoardType.equals(BoardType.Rectangle)) {
+
+            if (useTileShape.equals(TileShape.Square)) {
+
+            }
+
+            if (useTileShape.equals(TileShape.Octagonal)) {
+
+            }
+
+            if (useTileShape.equals(TileShape.Hexagonal)) {
+
+            }
+
+        }
+
+        // Regular polygon / convex shaped board (use side length to determine widest row width)
+        if (useBoardType.equals(BoardType.RegularPolygon)) {
+            
+            if (useTileShape.equals(TileShape.Square)) {
+
+            }
+
+            if (useTileShape.equals(TileShape.Octagonal)) {
+
+            }
+
+            if (useTileShape.equals(TileShape.Hexagonal)) {
+                
+            }
+
+        }
+        
     }
+
 }
