a43e3dd615877f6930a696b04251c15ab18809cb
NaJ64 <njudice@gmail.com>
2018-04-12 21:15:36 -0500

    added build rectangle method to board-generator

--- Ballast.Core/src/Client/services/board-generator.ts
+++ Ballast.Core/src/Client/services/board-generator.ts
@@ -8,51 +8,62 @@ import { IOffsetCoordinates, OffsetCoordinates} from '../models/offset-coordinat
 
 export class BoardGenerator {
 
-    public createBoard(tileShape: ITileShape, boardType: IBoardType, sizeWidth: number, sizeHeight?: number) {
+    public createBoard(tileShape: TileShape, boardType: IBoardType, sizeWidth: number, sizeHeight?: number) {
 
+        // Determine shape and tile layout
         let useTileShape = TileShape.fromObject(tileShape);
         let useBoardType = BoardType.fromObject(boardType);   
 
+        // Calculate width and height (number of tiles on board)
         let width = sizeWidth;
         let height = sizeHeight || sizeWidth;  
         if (useBoardType.equals(BoardType.RegularPolygon) && sizeWidth != sizeHeight) {
             throw new Error('Regular polygon board(s) cannot specify different width/height values');
         }
 
-        // Rectangular board (fixed number of tiles per row)
+        // Array to store rows of tiles (board)
+        let tiles: ITile[][] = [];
+
+        // Build Rectangular board (fixed number of tiles per row)
         if (useBoardType.equals(BoardType.Rectangle)) {
-
-            if (useTileShape.equals(TileShape.Square)) {
-
-            }
-
-            if (useTileShape.equals(TileShape.Octagonal)) {
-
-            }
-
-            if (useTileShape.equals(TileShape.Hexagonal)) {
-
-            }
-
+            tiles = this.buildRectangle(width, height, tileShape);
         }
 
-        // Regular polygon / convex shaped board (use side length to determine widest row width)
+        // Build Regular polygon / convex shape board
         if (useBoardType.equals(BoardType.RegularPolygon)) {
-            
             if (useTileShape.equals(TileShape.Square)) {
 
             }
-
             if (useTileShape.equals(TileShape.Octagonal)) {
 
             }
-
             if (useTileShape.equals(TileShape.Hexagonal)) {
                 
             }
-
         }
         
+    }
+
+    private buildRectangle(width: number, height: number, tileShape: TileShape) {
+        let rectangle: Tile[][] = [];
+        let increment = tileShape.doubleIncrement ? 2 : 1;
+        for(let rowIndex = 0; rowIndex < height; rowIndex++) {
+            let row = rowIndex * increment;
+            let newRow: Tile[] = [];
+            for (let colIndex = 0; colIndex < width; colIndex++) {
+                let col = colIndex * increment;
+                let cubicCoordinates = CubicCoordinates.fromOffset(
+                    OffsetCoordinates.fromObject({ row: row, col: col })
+                );
+                let newTile = Tile.fromObject({
+                    cubicCoordinates: cubicCoordinates,
+                    tileShape: tileShape
+                });
+                newRow.push(newTile);
+            }
+            rectangle.push(newRow);
+        }
+        return rectangle;
     }
 
 }
