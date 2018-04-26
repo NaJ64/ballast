ddb11139a79b92d927360136e0fa72f2c946b6ce
Nate Judice <njudice@gmail.com>
2018-04-26 11:51:47 -0500

    Added electron client/host

---
+++ Ballast.Electron/src/app.ts
@@ -0,0 +1,8 @@
+import 'reflect-metadata';
+import { BallastBootstrapper } from 'ballast-client';
+
+let server = 'https://ballast.azurewebsites.net'
+let bootstrapper = new BallastBootstrapper(document)
+    .bootstrapAsync(server)
+    .then(client => console.log('ballast loaded!'))
+    .catch((error: Error) => console.log('error loading ballast: ' + error.message));
