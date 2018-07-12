327e92f0098ee88280b550eaaecfcc32bb984eca
NaJ64 <njudice@gmail.com>
2018-07-12 00:31:15 -0500

    trying an update to appveyor.yml

--- ballast-electron/src/index.ts
+++ ballast-electron/src/index.ts
@@ -3,5 +3,5 @@ import { BallastBootstrapper } from 'ballast-client';
 let server = 'https://ballast.azurewebsites.net'
 let bootstrapper = new BallastBootstrapper(document)
     .bootstrapAsync(server)
-    .then(client => console.log('ballast loaded!'))
-    .catch((error: Error) => console.log('error loading ballast: ' + error.message));
+        .then(client => console.log('ballast loaded!'))
+        .catch((error: Error) => console.log('error loading ballast: ' + error.message));
