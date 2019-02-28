26a3e258ea2ecbe1b54e7af12a8acbc2590170e0
NaJ64 <njudice@gmail.com>
2019-02-28 01:21:03 -0600

    Change board defaults in web startup

--- ballast-web/dotnet/src/Startup.cs
+++ ballast-web/dotnet/src/Startup.cs
@@ -18,9 +18,9 @@ namespace Ballast.Web
             services.AddBallastServer(options => 
             {
                 options.UseSignalR = true;
-                options.DefaultBoardSize = 5; // odd number for "Regular Polygon" & must be greater than 3
-                options.DefaultBoardType = "Rectangle"; // "Rectangle", "RegularPolygon"
-                options.DefaultTileShape = "Square"; // "Hexagon", "Circle", "Square", "Octagon"
+                options.DefaultBoardSize = 7; // Must be an odd number for "Regular Polygon"; Must be greater than 3
+                options.DefaultBoardType = "RegularPolygon"; // "Rectangle", "RegularPolygon"
+                options.DefaultTileShape = "Hexagon"; // "Hexagon", "Circle", "Square", "Octagon"
                 options.DefaultLandToWaterRatio = 0.33;
             });
         }
