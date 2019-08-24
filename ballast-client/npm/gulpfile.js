dd2fbd6c38d781a0ba7908a09f00ac3b71a00278
NaJ64 <njudice@gmail.com>
2019-08-24 09:36:29 -0500

    Add new style gulpfile to client package

--- ballast-client/npm/gulpfile.js
+++ ballast-client/npm/gulpfile.js
@@ -1,35 +1,27 @@
 const del = require("del");
-const gulp = require("gulp");
+const { watch, series, dest } = require("gulp");
 const merge = require("merge2");
 const ts = require("gulp-typescript");
 const tsProject = ts.createProject("tsconfig.json", { "declaration": true });
 
-gulp.task("clean", () => {
+function clean() {
     return del(["./lib/**"]);
-})
+}
 
-gulp.task("compile:ts", () => {
+function compileTs() {
+    const destDir = "lib";
     const tsResult = tsProject.src().pipe(tsProject());
     return merge([
-        tsResult.js.pipe(gulp.dest("lib")),
-        tsResult.dts.pipe(gulp.dest("lib"))
+        tsResult.js.pipe(dest(destDir)),
+        tsResult.dts.pipe(dest(destDir))
     ]);
-});
+}
 
-gulp.task("bundle:ts", () => {
-    return Promise.resolve(); // Perform bundling here
-});
+function watchTs() {
+    watch("src/*.ts", compileTs);
+}
 
-gulp.task("compile", gulp.parallel("compile:ts"));
-
-gulp.task("bundle", gulp.parallel("bundle:ts"));
-
-gulp.task("build", gulp.series("clean", "compile", "bundle"));
-
-gulp.task("watch:ts", () => {
-    gulp.watch("src/*.ts", gulp.series("compile:ts", "bundle:ts"))
-});
-
-gulp.task("watch", gulp.series("build", gulp.parallel("watch:ts")));
-
-gulp.task("default", gulp.series("build"));
+exports.build = series(clean, compileTs);
+exports.clean = clean;
+exports.watch = series(clean, compileTs, watchTs);
+exports.default = series(clean, compileTs);
